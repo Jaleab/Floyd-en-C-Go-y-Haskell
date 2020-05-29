77704031c8b5ff16ce3ed9c4de9e2f68ceccc8d0
Jaleab <12jaaj01@hotmail.es>
2020-05-29 02:48:37 -0600

    Floy con grafos como input y con evaluacion forzada

--- Grafos/Floyd.hs
+++ Grafos/Floyd.hs
@@ -1,15 +1,21 @@
+
 -- join para retornar algo despues de I/O
-import Control.Monad (join)
+import Control.Monad (join, forM_)
 import Data.List (union)
-import Data.Map hiding (foldr, union)
+-- import Data.Pair 
+import Data.Map as Map hiding (foldr, union, map, foldl)
 import Data.Maybe (fromJust, isJust)
+--import Data.FiniteMap (insertList)
 import Data.Semigroup
-import Prelude hiding (lookup, filter)
+import Prelude hiding (lookup, filter, null)
+import System.Environment (getArgs)
+import Data.List.Split (chunksOf)
 
 data Shortest b a = Shortest { distance :: a, path :: [b] }
                   deriving Show
 
 instance (Ord a, Eq b) => Semigroup (Shortest b a) where
+  -- <> mappend: sirve para unir
   a <> b = case distance a `compare` distance b of
     GT -> b
     LT -> a
@@ -45,12 +51,41 @@ g = fromList [((2,1), 4)
              ,((2,3), 3)
              ,((1,3), -2)
              ,((3,4), 2)
-             ,((4,2), -1)]
-showShortestPaths v g = mapM_ print $ toList $ findMinDistances v g
+             ,((4,2), -1)
+             , ((5,5), 0)]
+--showShortestPaths v g = mapM_ print $ toList $ findMinDistances v g
+showShortestPaths v g = toList $ (findMinDistances v g)
+
+--graph = Map.empty
+
+
+
+insertList :: Ord key => [(key,elt)] -> Map key elt -> Map key elt
+insertList xs m = foldl (\m (k, v) -> Map.insert k v m) m xs
+
+main :: IO()
+main = do
+   args <- getArgs
+   content <- readFile (args !! 0)
+   let items = (map read).words
+   let str = map items (lines content) :: [[Int]]
+   let nodesQty = str!!0!!0
+   let edgesQty = str!!0!!1
+   let myList = tail str 
+   let edges = map  (\s -> ((s!!0, s!!1), s!!2)) myList
+   let graph = insertList edges Map.empty
+
+   let size = (length str) 
+   return $! showShortestPaths [0..300] (Sum <$> graph)
+   print size
+   -- Ejecucion
+   -- ghc Floyd.hs -o Floyd
+   -- ./Floyd sparse1.txt
+
 
 -- Ejecucion Linux despues de instalar Haskell 
 -- Creo que lo que falta es usar openFile o withFile para hacer el g (lista con Key:Value)
 -- gchi 
 -- :load Floyd
 -- Para Weights as distances 
--- showShortestPaths [1..4] (Sum <$> g) 
+-- showShortestPaths [1..4] (Sum <$> g)
